6e1f3f1c1a7c550a1ba75a4eb9c101e74b4a31d8
fgsargento629 <sargento228@gmail.com>
2023-12-09 11:58:07 +0000

    Implemented scraping to listing struct. Added todos to readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # realista
 Webscraper for supercasa.pt written in go (WIP)
+
+## TODO:
+
+### Priority
+
+- Detect that page doesnt exist, so the loop can end with no errors
+- property id as a uint, not string
+- func to get area and E rating from features string
+- Iterate through all of lisbon (maybe just each neighbourhood?)
+
+### Near future plans
+
+- Investigate analytics options in go (or maybe just use python)
+### Long term plans
+
+- Use plotly and dash to deploy analytics 
--- realista.go
+++ realista.go
@@ -5,22 +5,25 @@ import (
 	"log"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
 type Listing struct {
-	name, url, features, description string
-	lat, lon                         float32
-	price                            uint
+	name     string
+	features []string
+	lat, lon float64
+	price    uint
 }
 
-func scrape_supercasa() {
+func scrape_supercasa() []Listing {
 	fmt.Println("Scraping supercasa.pt")
 
 	// iterate through super casa website
 	// get all the listings for all the pages
 	max_pages2scrape := 5
+	var listings []Listing
 	for i := 0; i < max_pages2scrape; i++ {
 		c := colly.NewCollector()
 		url := "https://supercasa.pt/comprar-casas/lisboa/alvalade/pagina-" + strconv.Itoa(i)
@@ -55,6 +58,28 @@ func scrape_supercasa() {
 			fmt.Printf("Property Features: %s\n", features)
 			fmt.Printf("Latitude: %s\n", latitude)
 			fmt.Printf("Longitude: %s\n", longitude)
+
+			// type conversions from string to int
+			propertyPrice = strings.Split(propertyPrice, "\n")[0]
+			propertyPrice = strings.ReplaceAll(propertyPrice, ".", "")
+			propertyPrice = strings.TrimSuffix(propertyPrice, "€")
+			propertyPrice = strings.TrimSpace(propertyPrice)
+			propertyPriceInt, err := strconv.ParseUint(propertyPrice, 10, 64)
+			if err != nil {
+				log.Fatal("Price string to int conversion failed!")
+			}
+
+			latitudeF64, err := strconv.ParseFloat(latitude, 64)
+			if err != nil {
+				log.Fatal("Latitude string to float conversion failed!")
+			}
+
+			longitudeF64, err := strconv.ParseFloat(longitude, 64)
+			if err != nil {
+				log.Fatal("Longitude string to float conversion failed!")
+			}
+
+			listings = append(listings, Listing{name: propertyID, features: features, price: uint(propertyPriceInt), lat: latitudeF64, lon: longitudeF64})
 			fmt.Println("----------------------------------------")
 		})
 
@@ -64,9 +89,13 @@ func scrape_supercasa() {
 			log.Fatal(err)
 		}
 	}
+
+	return listings
 }
 
 func main() {
 	fmt.Println("Welcome to realista!")
-	scrape_supercasa()
+	listings := scrape_supercasa()
+	fmt.Println("Out of scrape supercasa!\n-------------------------------------------")
+	fmt.Println(listings[len(listings)-1])
 }
